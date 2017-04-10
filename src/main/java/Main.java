03bea211a59bbbc52206de5f2896d12e5ea7ec1f
Farshid Ashtari <farshid_11@hotmail.com>
2017-04-10 15:34:06 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by farshid on 4/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hi Yassin!");
+    }
+}
